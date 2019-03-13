99ce7c8377902c7c0c7e06e123d078392e12de65
Bruno Martins <bgcngm@gmail.com>
2019-03-13 21:00:33 +0000

    perseus: Import stock NFC configs
     * Configs taken from MIUI 9.3.7 global dev build
    
    Change-Id: Ie8149f4990fa85c0bb9ad7f6338c646d50b4d33b

--- device.mk
+++ device.mk
@@ -123,6 +123,10 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Power
 PRODUCT_PACKAGES += \
     power.qcom:64
