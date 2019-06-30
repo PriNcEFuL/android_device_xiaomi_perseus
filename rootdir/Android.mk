c8ea26e3f92340a04efa8c66bbbe5f1a2ef135ef
1nfrag <sil3ntcll@gmail.com>
2019-06-30 21:55:49 +0800

    perseus: Add device specific init script
     * Combined with init.brcm.rc
    
    Change-Id: I699df5cbe70b6fe93ee0ae1a4ac17b3d329c1a08

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -21,5 +21,5 @@ LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
