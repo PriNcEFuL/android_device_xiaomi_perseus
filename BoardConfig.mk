a898a028ab3e493c01fe5fdb8a500ecfacd18d31
subdragonzj <zj502407679@gmail.com>
2019-08-17 17:11:47 +0800

    perseus: manifest: set target-level=3
    Change-Id: Ia2fb539e01ede2786f5380caa8baa2c983dad84f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,7 +95,7 @@ TARGET_USES_ION := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(LOCAL_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(LOCAL_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
 
 # NFC
 TARGET_USES_NQ_NFC := true
