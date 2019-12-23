544d8fd188c1830d4be5bef5c5ac8cabde97f5fb
subdragonzj <zj502407679@gmail.com>
2019-12-24 01:37:10 +0800

    perseus: update build fingerprint(9.10.21)
    Change-Id: Ied42be50d517387e2be5347fdd2c12b1ed868795

--- mokee_perseus.mk
+++ mokee_perseus.mk
@@ -16,10 +16,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/perseus/perseus:9/PKQ1.180729.001/V11.0.4.0.PEECNXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/perseus/perseus:10/QKQ1.190828.002/9.10.21:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="perseus-user 9 PKQ1.180729.001 V11.0.4.0.PEECNXM release-keys" \
+    PRIVATE_BUILD_DESC="perseus-user 10 QKQ1.190828.002 9.10.21 release-keys" \
     PRODUCT_NAME="perseus" \
     TARGET_DEVICE="perseus"
 
