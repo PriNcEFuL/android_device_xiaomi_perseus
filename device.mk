cd7842df213437023f3194000b4578f4c9d430d3
subdragonzj <zj502407679@gmail.com>
2019-12-29 02:16:49 +0800

    Revert "perseus: add libshim for camera"
    This reverts commit 1e4a7fea8455a88bd41b94e7b31b6c93bd0cc594.

--- device.mk
+++ device.mk
@@ -130,8 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    Snap \
-    libshim_camera
+    Snap
 
 # Common init scripts
 PRODUCT_PACKAGES += \
