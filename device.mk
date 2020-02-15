42ec1c8bea2626f178c7d9ead2d7e5db7cfbf2c6
subdragonzj <zj502407679@gmail.com>
2020-02-15 17:10:19 +0800

    perseus: Prebuilt camera HIDL HAL

--- device.mk
+++ device.mk
@@ -142,9 +142,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     Snap
 
 # Context Hub
